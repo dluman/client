b9dd82953c94b0398dd7b2103034abb54a7d4af8
dluman <dluman@allegheny.edu>
2025-07-27 14:35:19 -0400

    Clever trick to force evaluation of program output and expected outcome into types

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Pointer
+# Client
 
-Make anything a plugin!
+Test a single or series of Python CLI programs all at once!
 
 ## Features
 
-Ever wanted to make another Python CLI program a plugin to yours? Now, you can!
+Ever needed to test a series of command line scripts with flexible YAML-based arguments? Me neither -- until I did!
 
 Provide a list of Python CLI invocations along with their associated command line arguments in the `config.yaml` (or other `YAML`) file, and go to it!
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
 import yaml
+import importlib
 import pkg_resources
+
+from io import StringIO
 
 def reset_args() -> None:
     """ Resets command-line arguments from prior binaries """
@@ -25,14 +28,37 @@ def discover_modules(modules: list = [], mod_group: str = "console_scripts") -> 
 
 def run_module(entry_point: pkg_resources.EntryPoint, arguments: list = []) -> None:
     """ Runs module based on reported entry point """
+
     # Apply any arguments provided in config
     for arg in arguments:
         if arg is not None:
             sys.argv.append(arg)
-    # Load the entry point (removes need for importlib)
-    entry = entry_point.load()
+
+    # Load the entry point or fail over to importlib
+    try:
+        entry = entry_point.load()
+    except ImportError:
+        mod = entry_point.attrs[0]
+        func = entry_point.attrs[1]
+        # Module apparently needs coercion
+        entry = getattr(importlib.import_module(
+            f"{entry_point.name}.{mod}"
+        ), func)
+
     # Run the program entry point
-    entry()
+    stdout = sys.stdout
+    output = StringIO()
+    sys.stdout = output
+    try:
+        # Run the entry point
+        entry()
+    except SystemExit as e:
+        # Circumvent any fun forced exits
+        pass
+    finally:
+        # Return stdout to original handler
+        sys.stdout = stdout
+    return output.getvalue()
 
 def main():
     """ Main function """
@@ -42,7 +68,19 @@ def main():
     # For each discovered module, run the process
     for entry in entry_points:
         # Run the module by providing the module entry and the args
-        run_module(entry_point = entry[0], arguments = entry[1])
+        result = run_module(
+            entry_point = entry[0],
+            arguments = entry[1]
+        )
+        # Get the specified outcome of the process
+        outcome = config["modules"][entry[0].name]["outcome"]
+        # Attempt to assert it in various ways; need to make
+        # type independent?
+        try:
+            expected_type = type(outcome)
+            assert expected_type(result) == outcome
+        except AssertionError:
+            sys.exit(1)
         # Blast the previously-supplied args
         reset_args()
 
